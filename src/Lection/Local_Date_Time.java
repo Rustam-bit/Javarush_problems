42cb169f092a7fb8808e50c7944845c40fe20b59
Rustam-bit <mirzaev.ra@phystech.edu>
2024-06-09 18:23:38 +0300

    24/5 = 4 часа 48 минут

---
+++ src/Lection/Local_Date_Time.java
@@ -0,0 +1,10 @@
+package Lection;
+
+import java.time.LocalDateTime;
+
+public class Local_Date_Time {
+    public static void main(String[] args) {
+        LocalDateTime time = LocalDateTime.now();
+        System.out.println(time);
+    }
+}
