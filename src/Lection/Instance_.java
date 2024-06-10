9dce282d01312ec5cce658d9bbb672aa9a6d0809
Rustam-bit <mirzaev.ra@phystech.edu>
2024-06-10 13:09:37 +0300

    Нафига вообще нужен этот Instant, хоть я и прочитал, все же забуду

---
+++ src/Lection/Instance_.java
@@ -0,0 +1,10 @@
+package Lection;
+
+import java.time.Instant;
+
+public class Instance_ {
+    public static void main(String[] args) {
+        Instant time = Instant.now();
+        System.out.println(time);
+    }
+}
